a6fc7d76641766854fa3c8be7c64950f12de65ae
rails -v <c:sites>
2017-04-27 15:13:33 -0500

    ast

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,36 +1,37 @@
 class PhotosController < ApplicationController
 
-def index
+  def index
 
-  render("photos/index.html.erb")
-end
+    render("photos/index.html.erb")
+  end
 
-def new_form
+  def new_form
 
-  render("photos/new_form.html.erb")
-end
+    render("photos/new_form.html.erb")
+  end
 
-def create_row
+  def create_row
 
-  render("photos/create_row.html.erb")
-end
+    render("photos/create_row.html.erb")
+  end
 
-def show
+  def show
+    the_id = params[:da_id]
+    @my_photo = Photo.find(the_id)
+    render("photos/show.html.erb")
+  end
 
-  render("photos/show.html.erb")
-end
+  def edit_form
 
-def edit_form
+    render("photos/edit_form.html.erb")
+  end
 
-  render("photos/edit_form.html.erb")
-end
+  def update_row
+    render("photos/update_row.html.erb")
+  end
 
-def update_row
-  render("photos/update_row.html.erb")
-end
+  def destroy_row
 
-def destroy_row
-
-  render("photos/destroy_row.html.erb")
-end
+    render("photos/destroy_row.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 get("/photos", {:controller => "photos", :action => "index"})
 get("/photos/new", {:controller => "photos", :action => "new_form"})
 get("/create_photo", {:controller => "photos", :action => "create_row"})
-get("/photos/:id", {:controller => "photos", :action => "show"})
+get("/photos/:da_id", {:controller => "photos", :action => "show"})
 get("/photos/:id/edit", {:controller => "photos", :action => "edit_form"})
 get("/update_photo/:id", {:controller => "photos", :action => "update_row"})
 get("/delete_photo/:id", {:controller => "photos", :action => "destroy_row"})
