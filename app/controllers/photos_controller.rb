3e66ef62ec34452352be3051fb83984343136611
rails -v <c:sites>
2017-04-27 14:53:47 -0500

    ssss

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,33 @@ def index
 
   render("photos/index.html.erb")
 end
+
+def new_form
+
+  render("photos/new_form.html.erb")
+end
+
+def create_row
+
+  render("photos/create_row.html.erb")
+end
+
+def show
+
+  render("photos/show.html.erb")
+end
+
+def edit_form
+
+  render("photos/edit_form.html.erb")
+end
+
+def update_row
+  render("photos/update_row.html.erb")
+end
+
+def destroy_row
+
+  render("photos/destroy_row.html.erb")
+end
 end
