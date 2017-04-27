611f356c6735c9151f7036499e192fba718df6ea
rails -v <c:sites>
2017-04-27 14:35:44 -0500

    ddsds

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,3 +1,7 @@
 class PhotosController < ApplicationController
 
+def index
+
+  render("photos/index.html.erb")
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get ("/photos", {:controller => "photos", :action => "index"})
-
+get("/photos", {:controller => "photos", :action => "index"})
+get("/photos/new", {:controller => "photos", :action => "new_form"})
+get("/create_photo", {:controller => "photos", :action => "create_row"})
+get("/photos/:id", {:controller => "photos", :action => "show"})
+get("/photos/:id/edit", {:controller => "photos", :action => "edit_form"})
+get("/update_photo/:id", {:controller => "photos", :action => "update_row"})
+get("/delete_photo/:id", {:controller => "photos", :action => "destroy_row"})
 end
