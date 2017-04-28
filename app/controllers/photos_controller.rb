830c201b9a639e843d4302d083c7a664f0cde67c
rails -v <c:sites>
2017-04-27 20:44:16 -0500

    zaab

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -29,24 +29,18 @@ class PhotosController < ApplicationController
     render("photos/show.html.erb")
   end
 
-  def edit_form
-    @the_id = params[:id]
-    the_id = params[:id]
-    @my_photo = Photo.find(the_id)
-    # @url = @my_photo.source
-    # @cap = @my_photo.caption
-    url = params["ed_source"]
-    cap = params["ed_caption"]
-    edit_photo = Photo.find(the_id)
-    edit_photo.source = url
-    edit_photo.caption = cap
-    edit_photo.save
-# redirect_to("/photos/#{edit_photo.id}")
+
+def edit_form
+    @photo = Photo.find(params[:id])
     render("photos/edit_form.html.erb")
   end
 
   def update_row
-    render("photos/update_row.html.erb")
+    photo = Photo.find(params[:id])
+    photo.caption = params[:the_caption]
+    photo.source = params[:the_source]
+    photo.save
+    redirect_to("/photos/#{photo.id}")
   end
 
   def destroy_row
