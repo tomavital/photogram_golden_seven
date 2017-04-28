2a21d20cbccf96f678e3b7e8dfb437c0c45b7f86
rails -v <c:sites>
2017-04-28 12:15:34 -0500

    ,,,m

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+get("/", {:controller => "photos", :action => "index"})
 get("/photos", {:controller => "photos", :action => "index"})
 get("/photos/new", {:controller => "photos", :action => "new_form"})
 get("/create_photo", {:controller => "photos", :action => "create_row"})
