406061e4ddd0f7b2c6e35e4f2de95a5d5f44264a
rails -v <c:sites>
2017-04-28 13:13:16 -0500

    dsdsds

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,7 +20,7 @@ class PhotosController < ApplicationController
     new_photo.caption = cap
     new_photo.save
     # render("photos/create_row.html.erb")
-    redirect_to("/photos/#{new_photo.id}")
+    redirect_to("/photos")
   end
 
   def show
